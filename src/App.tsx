c60de0256699fe513a43ec65220f152810103151
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-01-16 15:53:51 +0300

    feat: working with "useState"

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 
+export type filterType = "All" | "Active" | "Completed"
 
 function App() {
-
-    // let tasks = [
-    //     {id: 1, title: "HTML&CSS", isDone: true},
-    //     {id: 2, title: "JS", isDone: false},
-    //     {id: 3, title: "React", isDone: true}
-    // ]
 
     let [tasks, setTasks] = useState([
         {id: 1, title: "HTML&CSS", isDone: true},
@@ -18,13 +13,32 @@ function App() {
     ])
 
     const deleteTasks = (taskId: number) => {
-        setTasks(tasks.filter(f=>f.id !== taskId))
+        setTasks(tasks.filter(f => f.id !== taskId))
     }
 
+    let [filter, setFilter] = useState("All")
+
+    let filteredTasks = tasks
+
+    if (filter === "Active") {
+        filteredTasks = tasks.filter(f => !f.isDone)
+    }
+    if (filter === "Completed") {
+        filteredTasks = tasks.filter(f => f.isDone)
+    }
+
+    const filterTasks = (taskStatus: filterType) => {
+        setFilter(taskStatus)
+    }
 
     return (
         <div className={"App"}>
-            <Todolist title={"1"} arr={tasks} deleteTasks={deleteTasks}/>
+            <Todolist
+                title={"1"}
+                arr={filteredTasks}
+                deleteTasks={deleteTasks}
+                filterTasks={filterTasks}
+            />
         </div>
     )
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {filterType} from "./App";
 
 type TodolistPropsType = {
     title?: string
     arr: Array<inArray>
     deleteTasks: (taskId: number) => void
+    filterTasks: (taskStatus: filterType) => void
 }
 
 type inArray = {
@@ -11,7 +13,6 @@ type inArray = {
     title: string
     isDone: boolean
 }
-
 
 export function Todolist(props: TodolistPropsType) {
     return (
@@ -22,16 +23,16 @@ export function Todolist(props: TodolistPropsType) {
                 <button>+</button>
             </div>
             <ul>
-                {props.arr.map(m =>
-                    <li>
+                {props.arr.map((m, i) =>
+                    <li key={i}>
                         <button onClick={() => props.deleteTasks(m.id)}>delete</button>
                         <input type={"checkbox"} checked={m.isDone}/><span>{m.title}</span></li>
                 )}
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => props.filterTasks("All")}>All</button>
+                <button onClick={() => props.filterTasks("Active")}>Active</button>
+                <button onClick={() => props.filterTasks("Completed")}>Completed</button>
             </div>
         </div>
     )
