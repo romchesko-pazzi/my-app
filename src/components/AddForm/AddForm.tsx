d62e4756f5a3ed1399421314ad3dcbd2aca25e47
romchesko-pazzi <87422985+romchesko-pazzi@users.noreply.github.com>
2022-02-23 18:30:18 +0300

    feat: add addForm XD

---
+++ src/components/AddForm/AddForm.tsx
@@ -0,0 +1,43 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {Button} from "../Button/Button";
+import s from "../../Todolist.module.css"
+
+type AddFormPropsType = {
+    callback: (title: string) => void
+}
+
+export const AddForm = (props: AddFormPropsType) => {
+    const {callback} = props
+    const [value, setValue] = useState("");
+    const [error, setError] = useState(false);
+
+
+    const onClickHandler = () => {
+        if (value.trim() !== "") {
+            callback(value.trim());
+            setValue("");
+        }
+        if (value === "") {
+            setError(true);
+        }
+    }
+
+    const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === "Enter") {
+            onClickHandler()
+        }
+    }
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setError(false);
+        setValue(event.currentTarget.value);
+    }
+
+    return (
+        <div>
+            <input value={value} onChange={onChangeHandler} onKeyPress={onKeyPressHandler}/>
+            <Button name={"add TD"} callBack={onClickHandler}/>
+            {error ? <div className={s.errorMessage}>Title is required</div> : ""}
+        </div>
+    );
+};
